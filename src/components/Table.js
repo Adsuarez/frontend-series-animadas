47b864acacbded6ef4b4eaed06d7465609b1372a
Adsuarez <adsuarezq@gmail.com>
2024-07-10 18:36:24 -0500

    create and style table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,23 @@
 "use client";
+import { useEffect, useState } from "react";
 import styles from "./table.module.css";
+import { deleteDirector, readDirectors } from "@/services/directors";
 
 export default function Table({ title }) {
-  console.log({ title });
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    readDirectors().then((res) => setList(res));
+  }, []);
+
+  const updateItem = async (id) => {
+    console.log(`actualizando item ${id}`);
+  };
+
+  const deleteItem = async (id) => {
+    deleteDirector({ id }).then((res) => setList(res));
+  };
+
   return (
     <table className={styles.table}>
       <caption>lista de {title}</caption>
@@ -20,13 +35,23 @@ export default function Table({ title }) {
         </tr>
       </thead>
       <tbody className={styles.tbody}>
-        <tr>
-          <th className={styles.th} scope="row">
-            Chris
-          </th>
-          <td className={styles.td}>HTML tables</td>
-          <td>22</td>
-        </tr>
+        {list.map((item) => {
+          return (
+            <tr key={item.id}>
+              <th className={styles.th} scope="row">
+                {item.name}
+              </th>
+              <td className={styles.td}>{item.lastname}</td>
+              <td className={styles.td}>{item.country}</td>
+              <td className={styles.button} onClick={() => updateItem(item.id)}>
+                ✏️
+              </td>
+              <td className={styles.button} onClick={() => deleteItem(item.id)}>
+                ❌
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
       <tfoot>
         <tr>
