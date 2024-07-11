e1662c8d281d30a96ef9b4caa77410d3fe5cf99a
Adsuarez <adsuarezq@gmail.com>
2024-07-11 08:54:50 -0500

    fix error reasigning directos filtered to directors variable

--- src/services/directors.js
+++ src/services/directors.js
@@ -1,7 +1,7 @@
-const directors = [
-  { id: "1", name: "pepito", lastname: "perez", country: "colombia" },
-  { id: "2", name: "paco", lastname: "rubio", country: "peru" },
-  { id: "3", name: "pedro", lastname: "guaca", country: "mexico" },
+let directors = [
+  { id: "A", name: "pepito", lastname: "perez", country: "colombia" },
+  { id: "B", name: "paco", lastname: "rubio", country: "peru" },
+  { id: "C", name: "pedro", lastname: "guaca", country: "mexico" },
 ];
 
 export const readDirectors = async () => {
@@ -9,5 +9,6 @@ export const readDirectors = async () => {
 };
 
 export const deleteDirector = async ({ id }) => {
-  return directors.filter((director) => director.id !== id);
+  directors = directors.filter((director) => director.id !== id);
+  return directors;
 };
