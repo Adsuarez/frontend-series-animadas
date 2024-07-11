f5051d7ded33549474ff6a5d8ad91690bcdf7a29
Adsuarez <adsuarezq@gmail.com>
2024-07-11 11:40:26 -0500

    add save button and function and names in inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,49 @@
 import styles from "./form.module.css";
 
-export default function Form({ setShowForm }) {
+export default function Form({ setShowForm, setDataToSave, dataToSave }) {
   const close = async (event) => {
     event.preventDefault();
     setShowForm(false);
   };
 
+  const save = async (event) => {
+    event.preventDefault();
+    const data = Object.fromEntries(new FormData(event.target));
+    setDataToSave({ ...dataToSave, ...data });
+    setShowForm(false);
+  };
+
   return (
-    <form action="" method="get" className={styles.form}>
+    <form
+      action=""
+      method="get"
+      className={styles.form}
+      onSubmit={(event) => save(event)}
+    >
       <div>
         <label>
           Nombre
-          <input type="text" />
+          <input type="text" name="name" />
         </label>
       </div>
       <div>
         <label>
           Apellido
-          <input type="text" />
+          <input type="text" name="lastname" />
         </label>
       </div>
       <div>
         <label>
           País
-          <input type="text" />
+          <input type="text" name="country" />
         </label>
       </div>
 
       <button className={styles.close} onClick={(event) => close(event)}>
         X
       </button>
+
+      <button className={styles.save}>✔️</button>
     </form>
   );
 }
