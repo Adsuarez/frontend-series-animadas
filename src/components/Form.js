8d8e38ae14f1c6a0a9e0caaf3cfc1dd4c66beccf
Adsuarez <adsuarezq@gmail.com>
2024-07-11 10:15:07 -0500

    update form component with real fields, prop to change state and close function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,35 @@
-export default function Form() {
+import styles from "./form.module.css";
+
+export default function Form({ setShowForm }) {
+  const close = async (event) => {
+    event.preventDefault();
+    setShowForm(false);
+  };
+
   return (
-    <form>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+    <form action="" method="get" className={styles.form}>
+      <div>
+        <label>
+          Nombre
+          <input type="text" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Apellido
+          <input type="text" />
+        </label>
+      </div>
+      <div>
+        <label>
+          País
+          <input type="text" />
+        </label>
+      </div>
+
+      <button className={styles.close} onClick={(event) => close(event)}>
+        X
+      </button>
     </form>
   );
 }
