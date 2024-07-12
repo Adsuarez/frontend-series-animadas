c3b353b17e42390c1318448d0aa2d4186f14936e
Adsuarez <adsuarezq@gmail.com>
2024-07-12 17:08:33 -0500

    create toast messages constants and use to dinamic text

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,11 +1,22 @@
+import { ROUTES } from "@/constants";
 import styles from "./toast.module.css";
-export default function Toast({ action }) {
+const TOAST_CREATE_MESSAGE = {
+  [ROUTES.directors]: "director",
+  [ROUTES.genres]: "género",
+};
+
+const TOAST_UPDATE_MESSAGE = {
+  [ROUTES.directors]: "Director",
+  [ROUTES.genres]: "Género",
+};
+
+export default function Toast({ action, pathname }) {
   let message = "";
   if (action === "create") {
-    message = `Nuevo director creado`;
+    message = `Nuevo ${TOAST_CREATE_MESSAGE[pathname]} creado`;
   }
   if (action === "update") {
-    message = `Director actualziado`;
+    message = `${TOAST_UPDATE_MESSAGE[pathname]} actualizado`;
   }
 
   return (
