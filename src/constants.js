c5bfe24c29f84d2c7aa846d4fda8edeeff0b182d
Adsuarez <adsuarezq@gmail.com>
2024-07-12 15:06:00 -0500

    add constants to replace columns o table components

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,23 @@ export const COLUMNS_TITLES_DICTIONARY = {
   lastname: "Apellido",
   country: "País",
 };
+
+export const SPANISH_TO_ENGLISH_FIELDS_DICTIONARY = {
+  [COLUMNS_TITLES_DICTIONARY.name]: "name",
+  [COLUMNS_TITLES_DICTIONARY.lastname]: "lastname",
+  [COLUMNS_TITLES_DICTIONARY.country]: "country",
+};
+
+export const FIELDS_PER_PAGE = {
+  "/directores": [
+    COLUMNS_TITLES_DICTIONARY.name,
+    COLUMNS_TITLES_DICTIONARY.lastname,
+    COLUMNS_TITLES_DICTIONARY.country,
+  ],
+  "/generos": [COLUMNS_TITLES_DICTIONARY.name],
+};
+
+export const KEYS_PER_PAGE = {
+  "/directores": ["name", "lastname", "country"],
+  "/generos": ["name"],
+};
