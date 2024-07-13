66a24b68c5a9a0bf57ec79236668cf1f96d18bc2
Adsuarez <adsuarezq@gmail.com>
2024-07-13 18:02:53 -0500

    implement logic to use selector html tag with options for stored fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,13 @@
-import { FIELDS_PER_PAGE, SPANISH_TO_ENGLISH_FIELDS } from "@/constants";
+import {
+  FIELDS_PER_PAGE,
+  ROUTES,
+  SPANISH_TO_ENGLISH_FIELDS,
+} from "@/constants";
 import styles from "./form.module.css";
+import { useEffect, useState } from "react";
+import { readHandler } from "@/services/handlers";
+
+const REUSABLE_FIELDS = ["genre", "country", "actors", "director"];
 
 export default function Form({
   setShowForm,
@@ -7,7 +15,33 @@ export default function Form({
   dataToSave,
   pathname,
 }) {
+  const [directors, setDirectors] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [actors, setActors] = useState([]);
+  const [group, setGroup] = useState({});
+
+  useEffect(() => {
+    //readHandler({ pathname: ROUTES.directors, setList: setDirectors });
+    //readHandler({ pathname: ROUTES.genres, setList: setGenres });
+    //readHandler({ pathname: ROUTES.countries, setList: setCountries });
+    //readHandler({ pathname: ROUTES.actors, setList: setActors });
+
+    readHandler({ pathname: ROUTES.directors, setList: setDirectors }).then(
+      () =>
+        readHandler({ pathname: ROUTES.genres, setList: setGenres }).then(() =>
+          setGroup({
+            ["director"]: directors,
+            ["genre"]: genres,
+            ["country"]: countries,
+            ["actors"]: actors,
+          })
+        )
+    );
+  }, [directors, genres, countries, actors]);
+
   const columns = FIELDS_PER_PAGE[pathname];
+
   const close = async (event) => {
     event.preventDefault();
     setShowForm(false);
@@ -15,12 +49,12 @@ export default function Form({
 
   const save = async (event) => {
     event.preventDefault();
-
+    const isAllDataFilled = true;
     const data = Object.fromEntries(new FormData(event.target));
 
-    const isAllDataFilled = Object.values(data).every(
+    /*const isAllDataFilled = Object.values(data).every(
       (value) => value.length > 0
-    );
+    );*/
 
     if (isAllDataFilled) {
       setDataToSave({ ...dataToSave, ...data });
@@ -29,6 +63,7 @@ export default function Form({
     setShowForm(false);
   };
 
+  console.log({ group });
   return (
     <form
       action=""
@@ -41,7 +76,23 @@ export default function Form({
           <div key={column}>
             <label>
               {column}
-              <input type="text" name={SPANISH_TO_ENGLISH_FIELDS[column]} />
+              {REUSABLE_FIELDS.some(
+                (field) => field === SPANISH_TO_ENGLISH_FIELDS[column]
+              ) ? (
+                <select name={SPANISH_TO_ENGLISH_FIELDS[column]}>
+                  <option value="">--Seleccione una opción--</option>
+                  {group["director"] &&
+                    group[SPANISH_TO_ENGLISH_FIELDS[column]].map((item) => {
+                      return (
+                        <option key={item.id} value="">
+                          {item.name}
+                        </option>
+                      );
+                    })}
+                </select>
+              ) : (
+                <input type="text" name={SPANISH_TO_ENGLISH_FIELDS[column]} />
+              )}
             </label>
           </div>
         );
