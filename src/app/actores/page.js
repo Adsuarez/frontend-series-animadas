95663e14cbe1949c7e30264a4752fd345e8572c5
Adsuarez <adsuarezq@gmail.com>
2024-07-13 20:03:23 -0500

    create country and actor pages

---
+++ src/app/actores/page.js
@@ -0,0 +1,11 @@
+import "../globals.css";
+import Table from "@/components/Table";
+
+export default function ActorPage() {
+  return (
+    <article>
+      <h1 className="titlePage">LISTA DE ACTORES</h1>
+      <Table />
+    </article>
+  );
+}
---
+++ src/app/paises/page.js
@@ -0,0 +1,11 @@
+import "../globals.css";
+import Table from "@/components/Table";
+
+export default function CountryPage() {
+  return (
+    <article>
+      <h1 className="titlePage">LISTA DE PAÍSES</h1>
+      <Table />
+    </article>
+  );
+}
