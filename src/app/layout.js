0c0ff48ee39ac4c89734bfcfd755c3034a6656f8
Adsuarez <adsuarezq@gmail.com>
2024-07-14 11:46:17 -0500

    add TODO task

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  //TODO: include a message with disclaimer about countries and genres information required before a creation of directors, actors and movies
   return (
     <html lang="en">
       <body className={inter.className}>
