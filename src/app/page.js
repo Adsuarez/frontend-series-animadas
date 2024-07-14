c4898450b49be863e7e5dc9e75d7350dc02136b7
Adsuarez <adsuarezq@gmail.com>
2024-07-14 16:18:05 -0500

    remove image to use as background on main

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function HomePage() {
@@ -6,14 +5,6 @@ export default function HomePage() {
     <main className={styles.main}>
       <h1 className={styles.title_1}>Siempre</h1>
       <h1 className={styles.title_2}>Cartoons</h1>
-      <Image
-        className={styles.landing_image}
-        src="/tom_and_jerry.webp"
-        width={300}
-        height={200}
-        alt="the cartoon tom and jerry"
-        priority={true}
-      />
     </main>
   );
 }
